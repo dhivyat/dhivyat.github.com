a1a7df2c029c71a6df215cca79073c52584d7307
dhivyat <dthirum1@asu.edu>
2010-09-09 11:53:44 -0700

    initial

---
+++ samplemapAppDelegate.m
@@ -0,0 +1,27 @@
+
+#import "samplemapAppDelegate.h"
+#import "samplemapViewController.h"
+
+@implementation samplemapAppDelegate
+
+@synthesize window;
+@synthesize viewController;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {    
+    
+    // Override point for customization after app launch    
+    [window addSubview:viewController.view];
+    [window makeKeyAndVisible];
+}
+
+
+- (void)dealloc {
+    [viewController release];
+    [window release];
+    [super dealloc];
+}
+
+
+@end
+
